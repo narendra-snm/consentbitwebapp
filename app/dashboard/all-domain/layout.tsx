32a4e56766e20ad31a30511eb7fdd9eb031663cb
reshma17 <reshma@seattlenewmedia.com>
2026-04-18 01:45:01 +0530

    updated all domain page

--- app/dashboard/all-domain/layout.tsx
+++ app/dashboard/all-domain/layout.tsx
@@ -1,9 +1,16 @@
-
+export const runtime = 'edge';
 import React from 'react'
+import Header from '../components/header';
+import DashboardTabs from '../components/DashboardTabs';
 
 export default function layout({children}:{children:React.ReactNode}) {
   return (
     <>
+    <Header/>
+     <div className="max-w-[1148px] mx-auto pb-4.25">
+          <DashboardTabs/>
+          
+          </div>
       {children}
     </>
   )
--- app/dashboard/profile/layout.tsx
+++ app/dashboard/profile/layout.tsx
@@ -1,9 +1,11 @@
 
 import React from 'react'
+import Header from '../components/header'
 
 export default function layout({children}:{children:React.ReactNode}) {
   return (
     <>
+    <Header/>
       {children}
     </>
   )
