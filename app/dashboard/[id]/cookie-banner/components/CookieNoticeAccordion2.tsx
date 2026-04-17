ebf5e4857654734cb5add9dad98de2f460f5598b
Narendra chavan <narendra@seattlenewmedia.com>
2026-04-17 21:47:49 +0530

    removed lucid-react

--- app/dashboard/[id]/cookie-banner/components/CookieNoticeAccordion2.tsx
+++ app/dashboard/[id]/cookie-banner/components/CookieNoticeAccordion2.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+
 import Accordion from "./ui/Accordion";
 import ToggleSwitch from "./ui/ToggleSwitch";
 
--- app/dashboard/all-domain/page.tsx
+++ app/dashboard/all-domain/page.tsx
@@ -1,3 +1,4 @@
+
 export const runtime = 'edge';
 import { DomainManagementDashboard } from "./component/DomainManagementDashboard";
 
